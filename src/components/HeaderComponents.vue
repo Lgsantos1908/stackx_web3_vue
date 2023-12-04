59630c14bdf48e39c5d9813cfddf073d15bd9561
Luiz Gabriel Santos <lgsantos1908@gmail.com>
2023-12-04 13:44:01 -0300

    Ajuste largura header

--- src/components/HeaderComponents.vue
+++ src/components/HeaderComponents.vue
@@ -1,6 +1,6 @@
 <template>
     <header
-        class="bg-dark-400 flex items-center justify-centerm-0 py-4 px-20"
+        class="bg-dark-400 flex items-center justify-center m-0 py-4 px-20"
     >
         <img 
             src="../assets/images/stackX.svg" 
